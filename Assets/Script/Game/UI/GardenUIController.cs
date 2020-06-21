495f3d7d93b3f5a41248cbbffb1666da570eae20
KKKK <496007818@qq.com>
2020-06-21 23:22:50 +0800

    update:添加Ui

---
+++ Assets/Script/Game/UI/GardenUIController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GardenUIController : BaseUIObject
+{
+    private RectTransform Image;
+
+    private List<RectTransform> ImageList;
+    public override void Init()
+    {
+        Image = FindComponent<RectTransform>("Image");
+
+
+    }
+}
